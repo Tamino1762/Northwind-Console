9f78a3df5b3eeb9399c698dcc651fd3515567b3a
Tamino1762 <40547572+Tamino1762@users.noreply.github.com>
2019-11-11 19:37:34 -0600

    I'm hoping this is right!

--- Northwind-Console/Models/Category.cs
+++ Northwind-Console/Models/Category.cs
@@ -6,10 +6,15 @@ namespace NorthwindConsole.Models
     public class Category
     {
         public int CategoryId { get; set; }
-        [Required(ErrorMessage = "YO - Enter the name!")]
+
+        [Required(ErrorMessage = "YO - Enter the name!")]// with custom message
         public string CategoryName { get; set; }
+
+        //[StringLength(500)]//allows only 500 chars
         public string Description { get; set; }
 
-        public virtual List<Product> Products { get; set; }
+        // For Product ID
+        public virtual List<Product> Products { get; set; } // one to many category -> products
+        // (virtual = Relationship -- Doesn't load unless we want it)
     }
 }
--- Northwind-Console/Models/Supplier.cs
+++ Northwind-Console/Models/Supplier.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
 namespace NorthwindConsole.Models
 {
     public class Supplier
     {
         public int SupplierId { get; set; }
+        [Required]
         public string CompanyName { get; set; }
+        [Required]
         public string ContactName { get; set; }
         public string ContactTitle { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string Region { get; set; }
+        [Required]
         public string PostalCode { get; set; }
+        [Required]
         public string Country { get; set; }
+        [Required]
         public string Phone { get; set; }
         public string Fax { get; set; }
 
